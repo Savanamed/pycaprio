e440be66636fe622570484f4118d0701356ab87a
Sebmenke <smenke@savanamed.com>
2020-05-09 19:09:37 +0200

    Added test_curation_schema_dump_one_dict_many_false

--- tests/unit_tests/core/schemas/test_curation.py
+++ tests/unit_tests/core/schemas/test_curation.py
@@ -1,2 +1,8 @@
 from pycaprio.core.objects.curation import Curation
 from pycaprio.core.schemas.curation import CurationSchema
+
+
+def test_curation_schema_dump_one_dict_many_false(curation_schema: CurationSchema,
+                                                  deserialized_curation: Curation,
+                                                  serialized_curation: dict):
+    assert curation_schema.dump(deserialized_curation) == serialized_curation
