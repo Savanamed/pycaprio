f003d39bb3942577af07a93695ce2c0a7e1fc369
Sebmenke <smenke@savanamed.com>
2020-05-09 19:10:51 +0200

    Added test_curation_schema_dump_list_no_empty_many_true

--- tests/unit_tests/core/schemas/test_curation.py
+++ tests/unit_tests/core/schemas/test_curation.py
@@ -17,3 +17,8 @@ def test_curation_schema_dump_one_dict_many_true(curation_schema: CurationSchema
 def test_curation_schema_dump_list_many_true(curation_schema: CurationSchema,
                                              deserialized_curation: Curation):
     assert type(curation_schema.dump([deserialized_curation], many=True)) is list
+
+
+def test_curation_schema_dump_list_no_empty_many_true(curation_schema: CurationSchema,
+                                                      deserialized_curation: Curation):
+    assert len(curation_schema.dump([deserialized_curation], many=True)) == 1
