2392aa0c2ec5ab244013fb4e35f005960b38583f
Sebmenke <smenke@savanamed.com>
2020-05-09 19:10:02 +0200

    Added test_curation_schema_dump_one_dict_many_true

--- tests/unit_tests/core/schemas/test_curation.py
+++ tests/unit_tests/core/schemas/test_curation.py
@@ -6,3 +6,9 @@ def test_curation_schema_dump_one_dict_many_false(curation_schema: CurationSchem
                                                   deserialized_curation: Curation,
                                                   serialized_curation: dict):
     assert curation_schema.dump(deserialized_curation) == serialized_curation
+
+
+def test_curation_schema_dump_one_dict_many_true(curation_schema: CurationSchema,
+                                                 deserialized_curation: Curation,
+                                                 serialized_curation: dict):
+    assert curation_schema.dump([deserialized_curation], many=True) == [serialized_curation]
